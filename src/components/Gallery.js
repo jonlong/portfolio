0caffde224948455e88c0bcb050a3990b5e42af1
Jon Long <jon@linesandwaves.com>
2018-11-30 10:42:00 -0500

    re-enable impetus

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import { Flex } from 'rebass'
 import styled from 'react-emotion'
 import { rem } from 'polished'
+import Impetus from '../assets/js/vendor/impetus'
 import imagesLoaded from 'imagesloaded'
 
 const OverflowContainer = styled(Flex)`
@@ -10,7 +11,8 @@ const OverflowContainer = styled(Flex)`
   margin-bottom: ${rem('-30px')};
   padding-bottom: ${rem('30px')};
   position: relative;
-  touch-action: pan-x;
+  touch-action: pan-y;
+  -webkit-overflow-scrolling: touch;
 
   // use hardware acceleration
   transform: translateZ(0);
@@ -22,11 +24,49 @@ const ScrollBarClip = styled('div')`
   cursor: grab;
 `
 
-const Gallery = ({ children }) => (
-  <ScrollBarClip>
-    <OverflowContainer>{children}</OverflowContainer>
-  </ScrollBarClip>
-)
+class Gallery extends React.Component {
+  constructor(props) {
+    super(props)
+    this.impetusSource = React.createRef()
+  }
+
+  componentDidMount() {
+    const el = this.impetusSource.current
+
+    imagesLoaded(el, () => {
+      const maxScroll = el.scrollWidth - el.offsetWidth
+
+      new Impetus({
+        source: el,
+        boundX: [-maxScroll, 0],
+        bounce: true,
+        update: x => {
+          const leftPosition = -x
+          el.scrollLeft = leftPosition
+
+          // enable elastic effect past scroll boundaries with transforms
+          if (leftPosition < 0) {
+            el.style.transform = `translateX(${x}px)`
+          } else if (leftPosition > maxScroll) {
+            el.style.transform = `translateX(-${leftPosition - maxScroll}px)`
+          } else {
+            el.style.transform = 'translateX(0)'
+          }
+        },
+      })
+    })
+  }
+
+  render() {
+    return (
+      <ScrollBarClip>
+        <OverflowContainer innerRef={this.impetusSource}>
+          {this.props.children}
+        </OverflowContainer>
+      </ScrollBarClip>
+    )
+  }
+}
 
 Gallery.propTypes = {
   children: PropTypes.node.isRequired,
