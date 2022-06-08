d1c5e79b5de35af0b30030701022ee2442dd1b56
Jon Long <jon@jon.computer>
2022-06-08 17:07:18 -0400

    better styling for sublists

--- src/components/Markdown.js
+++ src/components/Markdown.js
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled'
 import { shevy } from '../styles/settings'
 import { modularScale } from 'polished'
+import { colors } from '../styles/settings'
 
 const Markdown = styled('div')`
   &:first-child {
@@ -36,6 +37,23 @@ const Markdown = styled('div')`
 
   ul li {
     list-style: disc;
+    margin-left: ${modularScale(0)};
+    padding-left: ${modularScale(-2)}
+  }
+
+  li {
+    ul {
+      margin-top: ${modularScale(1)};
+    }
+    li {
+      margin-bottom: 0;
+      margin-top: ${modularScale(0)};
+
+      &::marker {
+        color: ${colors.gray[1]}
+        content: "►";
+      }
+    }
   }
 `
 
